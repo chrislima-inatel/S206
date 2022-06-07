27476b5c18c3183d9bcdfdec5118372644195fc9
Chris Lima <christopher@inatel.br>
2022-06-07 18:59:29 -0300

    Element legth example

---
+++ cypress_testes/cypress/integration/aula_inatel/element_length.spec.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress"/>
+
+describe('Teste de tamanho de elementos!', () => {
+
+    it('Teste tamanho', () => { 
+        cy.visit('https://www.rahulshettyacademy.com/AutomationPractice/');
+        var tamanho = 0;
+        cy.get('fieldset > #product > tbody >').then(($el) => { //elemento que quero contar
+            const itemCount = Cypress.$($el).length;
+            tamanho = itemCount;
+            cy.log(tamanho);
+            cy.get('fieldset > #product > tbody >').should('have.length', tamanho);
+        })        
+    });
+
+});
+
